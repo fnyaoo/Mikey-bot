26abf590b2a85b64f99e0fd2be7f86b8f3bed1f4
fnyaoo <mr.eflukov@gmail.com>
2021-11-29 22:47:33 +0300

    edit admin role id

--- checks.py
+++ checks.py
@@ -5,5 +5,5 @@ def is_admin():
     async def predicate(ctx):
         if ctx.guild is None:
             raise NoPrivateMessage()
-        return ctx.author.id == 217998824671674368 or 821153000834859018 in [role.id for role in ctx.author.roles]
+        return ctx.author.id == 217998824671674368 or 885950589916286997 in [role.id for role in ctx.author.roles]
     return check(predicate)
